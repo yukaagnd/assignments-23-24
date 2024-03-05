330efe832c76dd9814b2fc7c99dd2bed1f5eeaf1
yukaagnd <89827900+yukaagnd@users.noreply.github.com>
2024-03-05 14:58:39 +0700

    FINAL

--- assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class OrderGenerator {
     private static final Scanner input = new Scanner(System.in);
 
+    // Method untuk mencetak main menu
     public static void showMenu(){
         System.out.println(">>=======================================<<");
         System.out.println("|| ___                 ___             _ ||");
@@ -20,6 +21,7 @@ public class OrderGenerator {
         System.out.println("3. Keluar");
     }
 
+    // Method untuk mencetak loop menu
     public static void showLoopMenu(){
         System.out.println("Pilih menu:");
         System.err.println("1. Generate Order ID");
@@ -27,6 +29,7 @@ public class OrderGenerator {
         System.out.println("3. Keluar");
     }
 
+    // Method untuk menghandle dan memvalidasi input pada generate ID
     public static String handleMenuGenerateID(){
         String namaResto;
         String tanggalOrder;
@@ -34,7 +37,9 @@ public class OrderGenerator {
         while (true){
             System.out.println();
             System.out.print("Nama Restoran: ");
+            // Menghilangkan spasi yang tidak diperlukan
             namaResto = input.nextLine().replace(" ", "");
+            // Memvalidasi input resto apakah panjangnya >= 4
             if (namaResto.length() < 4){
                 System.out.println("Nama Restoran tidak valid!");
                 continue;
@@ -42,11 +47,13 @@ public class OrderGenerator {
 
             System.out.print("Tanggal Pemesanan: ");
             tanggalOrder = input.nextLine();
+            // Memvalidasi input tanggal apakah sudah sesuai dengan format
             if (tanggalOrder.length() != 10 || tanggalOrder.charAt(2) != '/' || tanggalOrder.charAt(5) != '/'){
                     System.out.println("Tanggal pemesanan dalam format DD/MM/YYYY!");
                     continue;
             }
             
+            // Memvalidasi input no telepon apakah berupa digit
             System.out.print("No. Telpon: ");
             noTelepon = input.nextLine(); 
             if (!isNumeric(noTelepon)){
@@ -55,20 +62,25 @@ public class OrderGenerator {
             }
             break;
         }
+        // Memasukkan hasil input yang sudah divalidasi ke method generateOrderID
         return generateOrderID(namaResto, tanggalOrder, noTelepon);
     }
 
+    // Method untuk menyatukan kode OrderID sesuai ketentuan
     public static String generateOrderID(String namaRestoran, String tanggalOrder, String noTelepon) {
         String id = "";
         id = id + namaRestoran.substring(0, 4).toUpperCase();
         id = id + tanggalOrder.substring(0,2) + tanggalOrder.substring(3,5) + tanggalOrder.substring(6);
+        // Memanggil method cekDigitHP untuk menghitung digit yang akan dipakai
         id = id + cekDigitHP(noTelepon);
+        // Memanggil method cekSum untuk menghitung digit checksum yang akan dipakai
         id = id + cekSum(id);
 
         return id;
     }
 
-    public static void handleGenerateBill(){
+    //Method untuk menghandle dan memvalidasi input pada generate Bill
+    public static String handleGenerateBill(){
         String orderID;
         String lokasiPengiriman;
         
@@ -76,14 +88,16 @@ public class OrderGenerator {
             System.out.println();
             System.out.print("Order ID: ");
             orderID = input.nextLine();
+            // Memvalidasi order id agar panjangnya 16
             if (orderID.length() < 16){
                 System.out.println("Order ID minimal 16 karakter");
                 continue;
             }
             else if (orderID.length() > 16){
-                System.out.println("Silahkan masukkan Order ID yang valid!");
+                System.out.println("Order ID maksimal 16 karakter");
                 continue;
             }
+            // Memvalidasi order id agar ceksumnya sudah memenuhi syarat perhitungan
             else{
                 String ngecheckSum = cekSum(orderID.substring(0, 14));
                 if (orderID.substring(14).equals(ngecheckSum)){;}
@@ -92,6 +106,7 @@ public class OrderGenerator {
                     continue;
                 }
             }
+            // Memvalidasi lokasi agar lokasi yang dimasukkan valid
             System.out.print("Lokasi Pengiriman: ");
             lokasiPengiriman = input.nextLine().toUpperCase();
             if (lokasiPengiriman.equals("P") || lokasiPengiriman.equals("U") || lokasiPengiriman.equals("T") || lokasiPengiriman.equals("S") || lokasiPengiriman.equals("B")){;}
@@ -99,18 +114,21 @@ public class OrderGenerator {
                 System.out.println("Harap masukkan lokasi pengiriman yang ada pada jangkauan!");
                 continue;
             }
-            generateBill(orderID, lokasiPengiriman);
-            break;
+            // Memanggil method generateBill untuk mereturn output yang sesuai
+            System.out.println();
+            return generateBill(orderID, lokasiPengiriman);
         }
 
     }
 
+     // Method untuk menyatukan bill sesuai ketentuan
     public static String generateBill(String OrderID, String lokasi){
         String bill = "";
         bill += "Bill:\n";
         bill += "Order ID: " + OrderID + "\n";
         bill += "Tanggal Pemesanan: " + OrderID.substring(4,6) + "/" + OrderID.substring(6,8) + "/" + OrderID.substring(8,12) + "\n";
         bill += "Lokasi Pengiriman: " + lokasi.toUpperCase() + "\n";
+        // Mengkategorikan biaya ongkos kirim 
         switch (lokasi.toUpperCase()) {
             case "P":
                 bill += "Biaya Ongkos Kirim: Rp 10.000\n";
@@ -131,6 +149,7 @@ public class OrderGenerator {
         return bill;
     }
 
+    //Method main
     public static void main(String[] args) {
         showMenu();
         while(true) {
@@ -143,7 +162,7 @@ public class OrderGenerator {
             } 
             
             else if (command.equals("2")){
-                handleGenerateBill();
+                System.out.println(handleGenerateBill());
             } 
             
             else {
@@ -158,11 +177,13 @@ public class OrderGenerator {
         }
     }
 
+    // Method untuk mencari digitHP yang dipakai
     private static String cekDigitHP(String noTelepon){
         int digitHP = 0;
         int totalDigitHP = 0;
         int[] noHP;
         int panjangHP = (noHP = pengubahArray(noTelepon)).length;
+        // Menjumlahkan semua digit-digitnya
         for (int i = 0; i < panjangHP; i++){
             totalDigitHP = totalDigitHP + noHP[i];
         }
@@ -181,10 +202,12 @@ public class OrderGenerator {
         for (int i = 0; i < kode.length(); i++) {
             char kodeNow = kode.charAt(i);
             int asciiValue = (int) kodeNow;
-    
+            // Mencari nilai ascii dari tiap karakternya 
             if (i % 2 == 0) {
+                // Jika berupa angka, maka yang akan dijumlahkan adalah nilai dari angka itu sendiri
                 if (asciiValue < 58) {
                     genap += Character.getNumericValue(kodeNow);
+                // Jika berupa huruf maka yang akan dijumlahkan adalah nilai ascii dari huruf tersebut - 55
                 } else {
                     genap += asciiValue - 55;
                 }
@@ -218,7 +241,7 @@ public class OrderGenerator {
         return fixGenap + fixGanjil;
     }
 
-
+    // Method untuk ngecheck apakah isi String berupa angka 
     private static boolean isNumeric(String str) {
         char[] var4;
         int var3 = (var4 = str.toCharArray()).length;
@@ -232,6 +255,7 @@ public class OrderGenerator {
         return true;
     }
 
+    // Method untuk mengubah string menjadi array dengan tipe data integer
     public static int[] pengubahArray(String s) {
         int[] result = new int[s.length()];
         for (int i = 0; i < s.length(); i++) {
@@ -241,4 +265,3 @@ public class OrderGenerator {
     }
     
 }
-
